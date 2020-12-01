afa95115922296d4010c84a2f768bc2a47bad349
purisquiri <mau.dorigo@outlook.es>
2020-12-01 16:05:30 -0500

    show page json

--- app/controllers/practitioners_controller.rb
+++ app/controllers/practitioners_controller.rb
@@ -4,4 +4,10 @@ class PractitionersController < ApplicationController
         render json: practitioners, except: [:updated_at, :created_at]
     end
 
+    def show
+        practitioner = Practitioner.find(params[:id])
+        # practitioner = Practitioner.find_by(id: params[:id])
+
+        render json: practitioner, except: [:updated_at, :created_at]
+    end
 end
